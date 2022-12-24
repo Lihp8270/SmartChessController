3d81eb265a1601ab47ab7fc849f4e16ee8570bd2
Phil Dawson <phildawson@gmail.com>
2022-12-24 14:00:03 +0000

    Loop multiple files and display pin states

--- main.cpp
+++ main.cpp
@@ -1,54 +1,66 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
+// Shift register pins
 const uint load = 20;
 const uint clockEnable = 18;
-const uint dataIn = 14;
 const uint clockIn = 19;
 
+// Data input pins
+const uint firstDataPin = 14;
+const uint numOfFiles = 2;
+
+// Misc
 const uint led = 25;
 
-void initShiftRegPins() {
+void initPins() {
+    gpio_init(led);
+    gpio_set_dir(led, GPIO_OUT);
+
     gpio_init(load);
     gpio_init(clockEnable);
-    gpio_init(dataIn);
     gpio_init(clockIn);
 
     gpio_set_dir(load, GPIO_OUT);
     gpio_set_dir(clockEnable, GPIO_OUT);
     gpio_set_dir(clockIn, GPIO_OUT);
-    gpio_set_dir(dataIn, GPIO_IN);
 
-    gpio_pull_up(dataIn);
+    // Init all data input pins
+    gpio_init(15);
+    gpio_set_dir(15, GPIO_IN);
+    gpio_pull_up(15);
 }
 
 int main() {
     stdio_init_all();
-    gpio_init(led);
-    gpio_set_dir(led, GPIO_OUT);
-
-    initShiftRegPins();
+    
+    initPins();
 
     while(true) {
         sleep_ms(500);
         gpio_put(led, true);
         
-        // Pulse load pin to get data from parallel input
-        gpio_put(load, false);
-        sleep_ms(5);
-        gpio_put(load, true);
-        sleep_ms(5);
-
         // Get data from shift register
-        gpio_put(clockEnable, false);
+        for (int i = firstDataPin; i < (firstDataPin + numOfFiles); i++) {
+            // Pulse load pin to get data from parallel input
+            gpio_put(load, false);
+            sleep_ms(5);
+            gpio_put(load, true);
+            sleep_ms(5);
 
-        printf("Pin states:\n");
-        for (int i = 0; i<8; i++) {
-            gpio_put(clockIn, true);
-            printf("%d\n", gpio_get(dataIn));
-            gpio_put(clockIn, false);
+            gpio_put(clockEnable, false);      
+
+            printf("Pin %d states: ", i);
+            for (int k = 0; k < 8; k++) {
+                gpio_put(clockIn, true);
+                printf("%d", gpio_get(i));
+                gpio_put(clockIn, false);
+            }
+            gpio_put(clockEnable, true);
+            
+            printf("\n");
         }
-        gpio_put(clockEnable, true);
+        printf("\n");        
 
         sleep_ms(500);
         gpio_put(led, false);
